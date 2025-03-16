0d3feb600d1adc72603b633e863e62935703c9d2
Losdreyfedus <hamzakar13y@gmail.com>
2025-03-16 13:01:05 +0300

    Proje dosyası ekle.

---
+++ NvRFSOH1/DiziIslemleri.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class DiziIslemleri
+{
+    public static int[] DiziOlusturVeDoldur()
+    {
+        int[] sayilar = new int[5];
+
+        Console.WriteLine("5 adet sayı giriniz:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"{i + 1}. sayıyı girin: ");
+            sayilar[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        return sayilar;
+    }
+
+    public static void DiziyiYazdir(int[] dizi)
+    {
+        Console.WriteLine("Girdiğiniz sayılar:");
+
+        foreach (int sayi in dizi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+
+    public static int EnBuyukSayiyiBul(int[] dizi)
+    {
+        int enBuyuk = dizi[0];
+
+        for (int i = 1; i < dizi.Length; i++)
+        {
+            if (dizi[i] > enBuyuk)
+            {
+                enBuyuk = dizi[i];
+            }
+        }
+
+        return enBuyuk;
+    }
+}
---
+++ NvRFSOH1/Program.cs
@@ -0,0 +1,8 @@
+﻿// 2. soru: Kullanıcıdan alınan değerlerle dizi oluşturma ve yazdırma
+int[] kullaniciDizisi = DiziIslemleri.DiziOlusturVeDoldur();
+DiziIslemleri.DiziyiYazdir(kullaniciDizisi);
+
+// 3. soru: Dizideki en büyük sayıyı bulma
+int[] ornekDizi = { 12, 45, 6, 78, 23 };
+int enBuyukSayi = DiziIslemleri.EnBuyukSayiyiBul(ornekDizi);
+Console.WriteLine($"Dizideki en büyük sayı: {enBuyukSayi}");
