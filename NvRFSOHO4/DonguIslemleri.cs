cfe9972a8395f393fda8f94f81e71e8faeac221d
Losdreyfedus <hamzakar13y@gmail.com>
2025-03-16 13:19:01 +0300

    Yeni projeler ve döngü işlemleri eklendi

---
+++ NvRFSOHO4/DonguIslemleri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class DonguIslemleri
+{
+    public static void BirdenOnaKadarYazdir()
+    {
+        Console.WriteLine("1'den 10'a kadar olan sayılar:");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static int SayilarinToplaminiHesapla(int limit)
+    {
+        int toplam = 0;
+        for (int i = 1; i <= limit; i++)
+        {
+            toplam += i;
+        }
+        return toplam;
+    }
+}
---
+++ NvRFSOHO4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // 2. soru: 1'den 10'a kadar sayıları yazdırma
+        DonguIslemleri.BirdenOnaKadarYazdir();
+
+        // 3. soru: Kullanıcının girdiği sayıya kadar olan sayıların toplamını hesaplama
+        Console.Write("Bir sayı girin: ");
+        int kullaniciSayisi = Convert.ToInt32(Console.ReadLine());
+        int toplam = DonguIslemleri.SayilarinToplaminiHesapla(kullaniciSayisi);
+        Console.WriteLine($"1'den {kullaniciSayisi}'e kadar olan sayıların toplamı: {toplam}");
+    }
+}
